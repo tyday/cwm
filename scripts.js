ac51d75ceac1f09b5eb68dcd19c2c834bcb7a43e
Ty Day <tyrday@gmail.com>
2018-08-25 13:50:49 -0400

    add styling for header nav

--- scripts.js
+++ scripts.js
@@ -6,6 +6,13 @@ function scrollFunction(){
     document.documentElement.scrollTop > 80){
         document.getElementById("headerID").style.height = "50px";
         document.getElementById("logo").style.height = "50px";
-        document.getElementById("nav-links").style.height = "50px";
+        document.getElementById("nav-links").style.height = "40px";
+        document.getElementById("nav-links").style.padding = "10px 0 0 0";
+    }
+    else{
+        document.getElementById("headerID").style.height = "100px";
+        document.getElementById("logo").style.height = "100px";
+        document.getElementById("nav-links").style.height = "60px";
+        document.getElementById("nav-links").style.padding = "40px 0 0 0";
     }
 }
